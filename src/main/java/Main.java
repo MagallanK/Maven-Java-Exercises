4b3e2d474a231e215c883f43e514867084e35368
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-12 12:26:16 -0600

    Adding first maven-java-exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Main extends StringUtils {
+    public static void main(String[] args) {
+        System.out.println("Enter something: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered:  " + "\"" + userInput + "\"");
+        System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+
+    }
+
+}
