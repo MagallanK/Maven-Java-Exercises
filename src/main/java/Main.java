c4cf501aee0dc9c06753726249089da5dd7a9091
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-12 13:49:49 -0600

    Forgot to add the numeric method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,14 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
 
-public class Main extends StringUtils {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Enter something: ");
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
         System.out.println("You entered:  " + "\"" + userInput + "\"");
+        System.out.println("StringUtils.isNumeric(userInput) = " + StringUtils.isNumeric(userInput));
         System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
         System.out.println("Reversed: " + StringUtils.reverse(userInput));
 
