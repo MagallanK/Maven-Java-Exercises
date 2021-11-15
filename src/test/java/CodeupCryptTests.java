8b3b141fd933a8dc9b46d1db330b1fb0a3013fe6
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-15 12:22:03 -0600

    Completed CodeupCryptTests.java mini exercise

---
+++ src/test/java/CodeupCryptTests.java
@@ -0,0 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.charset.CoderMalfunctionError;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CodeupCryptTests {
+
+    @Test
+    public void testIfVersionIsSet(){
+        assertEquals(0.0, CodeupCrypt.version, 0);
+        CodeupCrypt.version = 1.2;
+        assertEquals(1.2, CodeupCrypt.version, 0);
+    }
+
+    @Test
+    public void testHashPassword(){
+        String password = "Password";
+        String expectedHash = "P4ssw0rd";
+
+        String hasedPassword = CodeupCrypt.hashPassword(password);
+
+        assertEquals(expectedHash, hasedPassword);
+        assertEquals("P13", CodeupCrypt.hashPassword("Pie"));
+        assertEquals("J9n1t", CodeupCrypt.hashPassword("JUnit"));
+    }
+
+    @Test
+    public void testCheckPassword(){
+        assertTrue("Password and hash are not the same.", CodeupCrypt.checkPassword("Magallan", "M4g4ll4n"));
+        assertFalse(CodeupCrypt.checkPassword("Magallan", "Magallan"));
+    }
+
+}
